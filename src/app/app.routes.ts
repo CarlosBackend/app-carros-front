7747c90ef313495cc5022309257b4c89310fc607
Carlos Henrique <carlinhosbackend@gmail.com>
2025-08-27 22:25:12 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/layout/login/login.component';
+import { PrincipalComponent } from './components/layout/principal/principal.component';
+import { CarrosListComponent } from './components/carros/carros-list/carros-list.component';
+import { CarrosdetailsComponent } from './components/carros/carrosdetails/carrosdetails.component';
+import { MarcaslistComponent } from './components/marca/marcaslist/marcaslist.component';
+import { MarcasdetailsComponent } from './components/marca/marcasdetails/marcasdetails.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'admin',
+    component: PrincipalComponent,
+    children: [
+      { path: 'carros', component: CarrosListComponent },
+      { path: 'carros/new', component: CarrosdetailsComponent },
+      { path: 'carros/edit/:id', component: CarrosdetailsComponent },
+      { path: 'marcas', component: MarcaslistComponent },
+      { path: 'marcas/new', component: MarcasdetailsComponent },
+      { path: 'marcas/edit/:id', component: MarcasdetailsComponent },
+    ],
+  },
+];
---
+++ src/app/components/carros/carros-list/carros-list.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { Carro } from '../../../models/carro';
+import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-carros-list',
+  imports: [RouterLink],
+  templateUrl: './carros-list.component.html',
+  styleUrl: './carros-list.component.scss',
+})
+export class CarrosListComponent {
+  lista: Carro[] = [];
+
+  constructor() {
+    this.lista.push(new Carro(1, 'Fiesta'));
+    this.lista.push(new Carro(2, 'Monza'));
+    this.lista.push(new Carro(3, 'Ka'));
+
+    let carroNovo = history.state.carroNovo;
+    let carroEditado = history.state.carroEditado;
+
+    if (carroNovo) {
+      carroNovo.id = 555;
+      this.lista.push(carroNovo);
+    }
+    if (carroEditado) {
+      let indice = this.lista.findIndex((x) => {
+        return x.id == carroEditado.id;
+      });
+      this.lista[indice] = carroEditado;
+    }
+  }
+  deleteById(carro: Carro) {
+    Swal.fire({
+      title: 'Tem certeza que deseja deletar esse registro?',
+      icon: 'warning',
+      showConfirmButton: true,
+      showDenyButton: true,
+      confirmButtonText: 'Sim',
+      cancelButtonText: 'Não',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let indice = this.lista.findIndex((x) => {
+          return x.id == carro.id;
+        });
+        this.lista.splice(indice, 1);
+        Swal.fire({
+          title: 'Salvo com sucesso!',
+          icon: 'success',
+          confirmButtonText: 'Ok',
+        });
+      }
+    });
+  }
+}
